d5ad66074c877107451558a473889cee12df7922
Harikrish9798 <91778286+Harikrish9798@users.noreply.github.com>
2021-10-08 04:42:09 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,18 @@
+
+public class Assignment1
+{
+
+	public static void main(String[] args){
+int number=1;		
+if(number>0){
+	System.out.println(number + "is a Positive");
+}
+else if (number < 0) {
+	System.out.println(number + "is a Negative");
+}
+else {
+	System.out.println(number + "is zero");
+}
+}
+
+}
