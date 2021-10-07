997f3459f00d16eae15f74d31320c90aa3a4ce2f
Harikrish9798 <91778286+Harikrish9798@users.noreply.github.com>
2021-10-08 04:45:18 +0530

    Add files via upload

---
+++ Primenumbers.java
@@ -0,0 +1,12 @@
+
+public class Primenumbers {
+	public static void main(Strings[] args) {
+		int input=16;
+		boolean flag=false;
+		for(int i=2; i<=8; i++) {
+			if(input%i==0) {
+				flag=true;
+				break;
+			}
+		}
+	}
